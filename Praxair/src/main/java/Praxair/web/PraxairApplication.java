841612e74245035f209008613f16804130525449
JordanAzo <jgodinez50085@ufide.ac.cr>
2025-07-08 13:57:34 -0600

    Avance

---
+++ Praxair/src/main/java/Praxair/web/PraxairApplication.java
@@ -0,0 +1,13 @@
+package Praxair.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraxairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraxairApplication.class, args);
+	}
+
+}
---
+++ Praxair/src/test/java/Praxair/web/PraxairApplicationTests.java
@@ -0,0 +1,13 @@
+package Praxair.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
